d11c9db9dcff0742e9ffd786258a959ebe71277e
Siddhant Gajanan Alchewar <117507571+sidalchewar@users.noreply.github.com>
2022-11-14 12:23:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app=express();  // will create app object from express package
 
 //Http Handlers
 var onDefault=function(req, res){
-    res.send("<h1>Transflower Learning Pvt. Ltd</h1>"+
+    res.send("<h1>Service Contoso</h1>"+
               "<hr/>"+
               "<ol>"+
                     " <li>Mentoring for skill building</li>"+
